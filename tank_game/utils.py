f7c0ee2173af2162fbec9579e71de150e2b80d64
Gaming32 <soujournme@gmail.com>
2020-12-18 19:47:52 -0600

    added missing function

--- tank_game/utils.py
+++ tank_game/utils.py
@@ -1,4 +1,7 @@
+from math import inf
+
 import pygame
+from pygame import Color, Surface, Vector2
 
 
 def rot_center(image: pygame.Surface, angle: int, x: int, y: int) -> tuple[pygame.Surface, pygame.Rect]:
@@ -7,3 +10,38 @@ def rot_center(image: pygame.Surface, angle: int, x: int, y: int) -> tuple[pygam
     new_rect = rotated_image.get_rect(center = image.get_rect(center = (x, y)).center)
 
     return rotated_image, new_rect
+
+
+def replace_image_colors(surf: Surface, colors: dict[tuple[int, int, int, int], Color]):
+    for x in range(surf.get_width()):
+        for y in range(surf.get_height()):
+            curcol = tuple(surf.get_at((x, y)))
+            if curcol in colors:
+                surf.set_at((x, y), colors[curcol])
+    return surf
+
+
+# Lifted from https://stackoverflow.com/a/32146853/8840278
+def raycast_line(ray_origin: Vector2, ray_direction: Vector2, pt1: Vector2, pt2: Vector2):
+    v1 = ray_origin - pt1
+    v2 = pt2 - pt1
+    v3 = Vector2(-ray_direction.y, ray_direction.x)
+
+    dot = v2 * v3
+    if abs(dot) < 0.000001:
+        return None
+    
+    t1 = v2.cross(v1) / dot
+    t2 = (v1 * v3) / dot
+
+    if t1 >= 0.0 and (t2 >= 0.0 and t2 <= 1.0):
+        return t1
+    
+    return None
+
+
+def raycast_line_distance(ray_origin: Vector2, ray_direction: Vector2, pt1: Vector2, pt2: Vector2):
+    pt_res = raycast_line(ray_origin, ray_direction, pt1, pt2)
+    if pt_res is None:
+        return inf
+    return ray_origin.distance_to(pt_res)
