2ff3578dd32b44f4e06aaf915c80e8855a97d0a2
Gaming32 <soujournme@gmail.com>
2020-12-22 17:07:43 -0600

    Revert "improved hit accuracy"
    This reverts commit 4052274f72b7cd8aa36eea9c9150eb3bcb7ae629.

--- tank_game/tank.py
+++ tank_game/tank.py
@@ -221,15 +221,10 @@ class Tank:
 
     def get_shot(self, tanks: list[Tank]) -> tuple[bool, float, Tank]:
         # 1,073,741,824 is 32,768*32,768 (this means we can shoot things up to 32,768 pixels away)
-        # did, dist, other = self.get_collision(96, 1073741824, self.turret_rotation, tanks)
+        did, dist, other = self.get_collision(96, 1073741824, self.turret_rotation, tanks)
         would_hit = False
-        minhitdist = inf
-        hitdist = inf
-        minother = None
-        # if did:
-        for other in tanks:
-            if other is self:
-                continue
+        hitdist = None
+        if did:
             pts = other.gethbox()
             would_hit = False
             turret_vec = Vector2()
@@ -242,11 +237,7 @@ class Tank:
                         break
                 if would_hit:
                     break
-            if hitdist is not None:
-                if hitdist < minhitdist:
-                    minhitdist = hitdist
-                    minother = other
-        return would_hit, minhitdist, minother
+        return would_hit, hitdist, other
 
     def shoot(self, surf: Surface, tanks: list[Tank], count_points=True):
         would_hit, hitdist, hitted = self.get_shot(tanks)
